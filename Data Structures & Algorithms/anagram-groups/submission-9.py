90d158332be4c207b42793b9fcc7efb796834b0f
CJ_KIM <rlatlswo13@gmail.com>
2026-07-16 23:20:00 +0900

    Add: anagram-groups - submission-9

---
+++ Data Structures & Algorithms/anagram-groups/submission-9.py
@@ -0,0 +1,17 @@
+import collections
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        hash_map = {}
+
+        for str in strs:
+            key = "".join(sorted(str))
+
+            if hash_map.get(key) is None:
+                hash_map[key] = [str]
+            else:
+                hash_map[key].append(str)
+
+        return [x for x in hash_map.values()]
+            
+        
