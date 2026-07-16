1cfca6690ef20622878e37f1e7beee37715874b8
CJ_KIM <rlatlswo13@gmail.com>
2026-07-16 18:38:29 +0900

    Add: two-integer-sum - submission-20

---
+++ Data Structures & Algorithms/two-integer-sum/submission-20.py
@@ -0,0 +1,11 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        table = {}
+
+        for idx,num in enumerate(nums):
+            t = target - num
+
+            if t in table:
+                return [table[t],idx]
+
+            table[num] = idx
