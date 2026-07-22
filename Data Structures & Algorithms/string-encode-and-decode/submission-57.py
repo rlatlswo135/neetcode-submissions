8c9c271f9c54381c85e4bf8f2fc4826472779ce5
CJ_KIM <rlatlswo13@gmail.com>
2026-07-22 18:17:59 +0900

    Add: string-encode-and-decode - submission-57

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-57.py
@@ -0,0 +1,36 @@
+MARKER = "&"
+C_MARKER = "-"
+class Solution:
+    def encode(self, strs: List[str]) -> str:
+        if not strs:
+            return ""
+
+        len_list = "".join([f"{len(x)}{C_MARKER}" for x in strs])
+        serial = "".join(strs)
+
+        return f"{len_list}{MARKER}{serial}"
+
+    def decode(self, s: str) -> List[str]:
+        if s == "":
+            return []
+
+        marker_idx = s.find(MARKER)
+
+        result = []
+        pointer = 0
+        c_pointer = 0
+
+        strs = s[marker_idx+1:]
+        counts = s[:marker_idx]
+
+        for idx,count in enumerate(counts):
+            if count == C_MARKER:
+                i_count = int(counts[c_pointer:idx])
+                word = strs[pointer:pointer + i_count]
+
+                result.append(word)
+                c_pointer = idx + 1
+                pointer += i_count
+                
+
+        return result
